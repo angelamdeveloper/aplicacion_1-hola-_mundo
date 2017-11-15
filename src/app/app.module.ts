0b2e13a5e9ec72611a5d041264844f5281082389
Unknown <angelamdeveloper@gmail.com>
2017-11-15 21:42:55 +0100

    fin de video 36

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { HeaderComponent} from "./components/header.component";
+import { HeaderComponent } from "./components/header.component";
+import { BodyComponent } from "./components/body.component";
+import { FooterComponent } from "./components/footer.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
